3fac73d87c8f46f7263f348c190697ee4525efa1
DESKTOP-J0C6LEP\LENONO 330S <michael.baez.mendoza1991@gmail.com>
2021-08-18 18:57:35 -0500

    servio_rest

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+// $db = [
+//     'host' => 'localhost',
+//     'username' => 'root',
+//     'password' => 'root',
+//     'db' => 'baseprueba' //Cambiar al nombre de tu base de datos
+// ];
+
+?>
